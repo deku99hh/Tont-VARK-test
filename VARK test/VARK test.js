9e6bfe76c9f17b9c9dc92175aa35043af365262d
AbdelrahmanRashed <103854033+deku99hh@users.noreply.github.com>
2025-08-31 17:31:18 +0300

    it works now!

--- VARK test/VARK test.js
+++ VARK test/VARK test.js
@@ -1,72 +1,106 @@
 // importing the qustions
 let qustionHTML = '';
+let qustionNumber = 0;
+let isAnswered = false;
 
-VARKQuestions.forEach((qustion, index) => {
-    qustionHTML +=`
-        <div class="QuestionBlook">
-            <p class="QuestionNum">Question ${index + 1 } of 20</p>
-            <p class="Question">${qustion.qustion}</p>
-            <div class="answers">
-                <div class="answerBlock">
-                        <span class="yello-circle"></span>
-                        <span class="TheAnswers" data-value="V">${qustion.answers.answerV}</span>
-                </div>
-                <div class="answerBlock">
-                        <span class="yello-circle"></span>
-                        <span class="TheAnswers" data-value="A">${qustion.answers.answerA}</span>
-                </div>
-                <div class="answerBlock">
-                        <span class="yello-circle"></span>
-                        <span class="TheAnswers" data-value="R">
-                            ${qustion.answers.answerR}
-                        </span>
-                </div>
-                <div class="answerBlock">
-                        <span class="yello-circle"></span>
-                        <span class="TheAnswers" data-value="K">
-                            ${qustion.answers.answerK}
-                        </span>
-                </div>
-            </div>
-        </div>
-
-    
-    `;
-});
-
-document.querySelector('main').innerHTML= qustionHTML;
-// end of importing the qustions
-
-// counting your VARK
 let Vcount = 0;
 let Acount = 0;
 let Rcount = 0;
 let Kcount = 0;
 
-document.querySelectorAll('.yello-circle').forEach(yelloCircle => {
-    yelloCircle.addEventListener('click', () => {
-        const answer = yelloCircle.nextElementSibling.dataset.value;
+loadQustions();
+function loadQustions(){
 
-        // if(document.querySelectorAll('.yello-circle').innerHTML === ''){console.log('fuck, man')}
+    if (qustionNumber>16){
+        qustionHTML = `
+            <div class="QuestionBlook">
+                <p class="QuestionNum">Test Completed</p>
+                <p class="Question">Your Learning Style is:</p>
+                <div class="answers">
+                    <div class="answerBlock TheAnswers" style="text-align: center; font-size: 20px;">
+                    نسبة قابلية تعلمك بشكل بصري: ${Math.round(Vcount/17*100)}%
+                    </div>
+                    <div class="answerBlock TheAnswers" style="text-align: center; font-size: 20px;">
+                    نسبة قابلية تعلمك بشكل بصري: ${Math.round(Acount/17*100)}%
+                    </div>
+                    <div class="answerBlock TheAnswers" style="text-align: center; font-size: 20px;">
+                    نسبة قابلية تعلمك بالقراءة/الكتابة: ${Math.round(Rcount/17*100)}%
+                    </div>
+                    <div class="answerBlock TheAnswers" style="text-align: center; font-size: 20px;">
+                    نسبة قابلية تعلمك بشكل حركي: ${Math.round(Kcount/17*100)}%
+                    </div>
+                </div>
+                <div style="display: flex; justify-content: center;"><button class="nextButton" onclick="
+                location.reload()
+                ">Retake Test</button></div>
+            </div>
+        `;
+        console.log(qustionNumber);
+        document.querySelector('main').innerHTML= qustionHTML;
 
+    }
+    else {
+        qustionHTML = `
+            <div class="QuestionBlook">
+                <p class="QuestionNum">Question ${qustionNumber + 1 } of 17</p>
+                <p class="Question">${VARKQuestions[qustionNumber].qustion}</p>
+                <div class="answers">
+                    <div class="answerBlock">
+                            <span class="yello-circle"></span>
+                            <span class="TheAnswers" data-value="V">${VARKQuestions[qustionNumber].answers.answerV}</span>
+                    </div>
+                    <div class="answerBlock">
+                            <span class="yello-circle"></span>
+                            <span class="TheAnswers" data-value="A">${VARKQuestions[qustionNumber].answers.answerA}</span>
+                    </div>
+                    <div class="answerBlock">
+                            <span class="yello-circle"></span>
+                            <span class="TheAnswers" data-value="R">
+                                ${VARKQuestions[qustionNumber].answers.answerR}
+                            </span>
+                    </div>
+                    <div class="answerBlock">
+                            <span class="yello-circle"></span>
+                            <span class="TheAnswers" data-value="K">
+                                ${VARKQuestions[qustionNumber].answers.answerK}
+                            </span>
+                    </div>
+                </div>
+                <div style="display: flex; justify-content: center;"><button class="nextButton" onclick="
+                if(isAnswered == true){
+                loadQustions()
+                }
+                ">next</button></div>
+            </div>
+            
+        `;
+        document.querySelector('main').innerHTML= qustionHTML;
+        qustionNumber=qustionNumber+1;
+        isAnswered = false;
 
-        if(answer === 'V'){
-            Vcount++;
-        }else if(answer === 'A'){
-            Acount++;
-        }else if(answer === 'R'){
-            Rcount++;
-        }else if(answer === 'K'){
-            Kcount++;
-        }
-        console.log(Vcount, Acount, Rcount, Kcount);
-        
-    });
-});
+        document.querySelectorAll('.yello-circle').forEach(yelloCircle => {
+        yelloCircle.addEventListener('click', () => {
+            if(isAnswered == false){
 
+                yelloCircle.style.backgroundColor = 'rgb(0, 0, 202)';
 
-// document.querySelectorAll('.yello-circle').forEach(yelloCircle => {
-//     yelloCircle.addEventListener('click', () => {
-        
-//     });
-// });
+                const answer = yelloCircle.nextElementSibling.dataset.value;
+
+                isAnswered = true;
+                if(answer === 'V'){
+                    Vcount++;
+                }else if(answer === 'A'){
+                    Acount++;
+                }else if(answer === 'R'){
+                    Rcount++;
+                }else if(answer === 'K'){
+                    Kcount++;
+                }
+                console.log(Vcount, Acount, Rcount, Kcount);
+                }
+
+            });
+        });
+    }
+
+};
